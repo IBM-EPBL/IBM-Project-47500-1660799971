fef433c37a9f2f405382634b0f341cda9909d1e9
praveena8602 <113662743+praveena8602@users.noreply.github.com>
2022-09-26 03:43:01 -0700

    Update temp &humidity.py

--- temp &humidity.py
+++ temp &humidity.py
@@ -1,6 +1,9 @@
-temperature = 35
-humidity = 90
-if temperature > 30 and humidity > 85:
-    print('Alarm on')
-else:
-    print('Alarm off')
+import random
+temperature = random.randint(100,150)
+humidity = random.randint(100,130)
+if temperature>150 and humidity>130:
+    print ('Alarm is on')
+temperature = random.randint(50,70)
+humidity = random.randint(30,80)
+if temperature<50 and humidity<30:
+        print ('Alarm is off')
