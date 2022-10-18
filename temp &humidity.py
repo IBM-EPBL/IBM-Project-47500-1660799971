abb4544ef319da9fab5096a85978c0611ff1c806
praveena8602 <113662743+praveena8602@users.noreply.github.com>
2022-10-17 22:30:07 -0700

    Delete temp &humidity.py

--- temp &humidity.py
+++
@@ -1,9 +0,0 @@
-import random
-temperature = random.randint(100,150)
-humidity = random.randint(100,130)
-if temperature>150 and humidity>130:
-    print ('Alarm is on')
-temperature = random.randint(50,70)
-humidity = random.randint(30,80)
-if temperature<50 and humidity<30:
-        print ('Alarm is off')
